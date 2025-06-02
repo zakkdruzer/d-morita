eff7620873055191f43ae7eac41cad91a0d2ded6
zakkdruzer <zakkdruzer@gmail.com>
2025-06-02 20:21:25 -0300

    cambios generales del frontened

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import { PetProvider } from './context/PetContext'; // <-- importa el provider
-import { AuthProvider } from './context/AuthContext'; // <-- si usas AuthProvider
+import App from './App';
+import { PetProvider } from './context/PetContext.tsx'; // <-- importa el provider
+import { AuthProvider } from './context/AuthContext.tsx'; // <-- si usas AuthProvider
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
